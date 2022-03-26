6b14ff4f839678ffa25cdec1140c78fae1c2e1e3
Numan Umer <numanchudary20@gmail.com>
2022-03-26 21:29:41 +0500

    SR-3: Changes the realted fields product_default_code and product_barcode to the product ddependent rather than template dependent, and added the barcode in Stock on hand view, Invnetory reporting

--- stock_ext/models/stock_quant.py
+++ stock_ext/models/stock_quant.py
@@ -4,11 +4,16 @@ from odoo import _, api, fields, models
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
-    product_categ_id = fields.Many2one(related='product_tmpl_id.categ_id', store=True,)
+    product_categ_id = fields.Many2one(related='product_id.categ_id', store=True,)
     
     product_default_code = fields.Char(
         string='Internal Reference',
-        related='product_tmpl_id.default_code',
+        related='product_id.default_code',
+        store=True,
+    )
+    product_barcode = fields.Char(
+        string='Barcode',
+        related='product_id.barcode',
         store=True,
     )
     
