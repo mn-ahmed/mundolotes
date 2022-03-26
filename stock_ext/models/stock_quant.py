a1e17c2484a9c90127eda4035f8e32749fde5763
Numan Umer <numanchudary20@gmail.com>
2022-03-26 21:12:07 +0500

    SR-3: Added the related field product_default_code (Internal Reference) on product in stock_quant model for inventory report view, Stock On Hand

--- stock_ext/models/stock_quant.py
+++ stock_ext/models/stock_quant.py
@@ -5,3 +5,10 @@ class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
     product_categ_id = fields.Many2one(related='product_tmpl_id.categ_id', store=True,)
+    
+    product_default_code = fields.Char(
+        string='Internal Reference',
+        related='product_tmpl_id.default_code',
+        store=True,
+    )
+    
