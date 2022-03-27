1bc69f8269491ee4a6228316ececde02fb7c3c8c
Numan Umer <numanchudary20@gmail.com>
2022-03-27 15:59:51 +0500

    SR-3: Implemented the Computation of Stock Value, Stock Value = Available Quantity    * Product Sales Price to be displayed in Stock on Hand Inventory Report

--- stock_ext/models/stock_quant.py
+++ stock_ext/models/stock_quant.py
@@ -16,14 +16,21 @@ class StockQuant(models.Model):
         related='product_id.barcode',
         store=True,
     )
-
     currency_id = fields.Many2one(
         string='Currency',
         comodel_name='res.currency',
         related='product_id.currency_id'
     )
-    
     product_list_price = fields.Float(
         string='Product Sales Price',
         related='product_id.list_price',
     )
+    stock_value = fields.Float(
+        string='Stock Value',
+        compute='compute_stock_value',
+        help='Stock Value = Available Quantity	* Product Sales Price'
+    )
+    @api.depends('product_list_price', 'available_quantity')
+    def compute_stock_value(self):
+        for record in self:
+            record.stock_value = record.product_list_price * record.available_quantity
