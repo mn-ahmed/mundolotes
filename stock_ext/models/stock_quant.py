0cc622ee9d96fca736aafda8cee1eac8e9adde87
Numan Umer <numanchudary20@gmail.com>
2022-03-27 15:41:19 +0500

    SR-3: Added the product_list_price related field to be displayed in Stock On Hand Inventory Report List View

--- stock_ext/models/stock_quant.py
+++ stock_ext/models/stock_quant.py
@@ -16,4 +16,14 @@ class StockQuant(models.Model):
         related='product_id.barcode',
         store=True,
     )
+
+    currency_id = fields.Many2one(
+        string='Currency',
+        comodel_name='res.currency',
+        related='product_id.currency_id'
+    )
     
+    product_list_price = fields.Float(
+        string='Product Sales Price',
+        related='product_id.list_price',
+    )
