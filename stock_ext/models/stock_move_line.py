13d77dd7b42962c076c6853414c75d90e7c4778d
Numan Umer <numanchudary20@gmail.com>
2022-04-08 02:18:56 +0500

    SR-12: Added the product price list field

--- stock_ext/models/stock_move_line.py
+++ stock_ext/models/stock_move_line.py
@@ -7,9 +7,20 @@ class StockMoveLine(models.Model):
     product_categ_id = fields.Many2one(
         string='Product Category',
         comodel_name='product.category',
-        related='product_id.categ_id'
+        related='product_id.categ_id',
+        store=True,
     )
     product_id_default_code = fields.Char(
         string='Internal Reference',
-        related='product_id.default_code'
+        related='product_id.default_code',
+        store=True,
+    )
+    currency_id = fields.Many2one(
+        string='Currency',
+        comodel_name='res.currency',
+        related='product_id.currency_id'
+    )
+    product_list_price = fields.Float(
+        string='Product Sales Price',
+        related='product_id.list_price',
     )
