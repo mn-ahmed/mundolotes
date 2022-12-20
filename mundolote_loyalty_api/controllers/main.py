266fc2a0ec0351b0c8fab2ae4f0e0acfe9e0740b
Jorge Mendoza <jjmendoza6@gmail.com>
2022-12-20 09:26:55 -0600

    init logger

--- mundolote_loyalty_api/controllers/main.py
+++ mundolote_loyalty_api/controllers/main.py
@@ -1,7 +1,9 @@
+import logging
 from odoo import http, _, tools
 from odoo.http import request
 from odoo.exceptions import UserError
 
+_logger = logging.getLogger(__name__)
 
 class LoyaltyController(http.Controller):
     @http.route('/api/loyalty-request/<string:cus_id>', type='http', auth='none', methods=["GET"], csrf=False)
