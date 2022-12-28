c62f0dbe83aec26713e7a18e66f04c6e3f5b8090
Jorge Mendoza <jjmendoza6@gmail.com>
2022-12-28 09:37:46 -0600

    post request

--- mundolote_loyalty_api/controllers/main.py
+++ mundolote_loyalty_api/controllers/main.py
@@ -8,15 +8,25 @@ from odoo.exceptions import UserError
 _logger = logging.getLogger(__name__)
 
 class LoyaltyController(http.Controller):
-    @http.route('/api/loyalty-request/<string:cus_id>', type='http', auth='none', methods=["GET"], csrf=False)
-    def get_balance(self, cus_id, **params):
+    @http.route('/api/loyalty-request', type='json', auth='none', methods=["POST"], csrf=False)
+    def get_balance(self, *args, **post):
+        key = "QM+jWC=4cb6d5!tHShGAKVUJNq1m2^Zv"
+        try:
+            vat = post["vat"]
+        except KeyError:
+            raise exceptions.AccessDenied(message='`vat` is required.')
+
+        try:
+            secret_key = post["secret_key"]
+        except KeyError:
+            raise exceptions.AccessDenied(message='Invalid Request.')
         try:
 
-            data = request.env['res.partner'].sudo().search([('phone', '=', cus_id)], limit=1)
+            data = request.env['res.partner'].sudo().search([('vat', '=', vat)], limit=1)
             _logger.info("response count: {0}".format(len(data)))
             customer = data[0]
 
-            res = {'id': cus_id, 'name': customer["name"], 'points': customer["loyalty_points1"]}
+            res = {'id': vat, 'name': customer["name"], 'points': customer["loyalty_points1"]}
             return http.Response(
                 json.dumps(res),
                 status=200,
